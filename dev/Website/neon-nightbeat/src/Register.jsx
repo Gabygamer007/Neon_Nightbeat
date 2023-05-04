ae38bc8a7af0b74f12c706de619e5e25095a48ca
NITNIT2 <matisgaetjens@gmail.com>
2023-05-04 10:13:02 -0400

    update

--- dev/Website/neon-nightbeat/src/Register.jsx
+++ dev/Website/neon-nightbeat/src/Register.jsx
@@ -1,23 +1,27 @@
 import React, { useState } from "react";
 
 export const Register = (props) => {
-    const [email, setUsername] = useState('');
-    const [pass, setPassword] = useState('');
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(email);
+        console.log(username);
     }
 
     return (
-        <div className="auth-form-container">
-            <h2>REGISTER</h2>
-        <form className="form" onSubmit={handleSubmit}>
-            <input value={email} onChange={(e) => setUsername(e.target.value)}type="username" placeholder="Username" id="username" name="username" />
-            <input value={pass} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="Password" id="password" name="password" />
-            <button className="register-btn" type="submit">REGISTER</button>
-        </form>
-        <button className="link-btn" onClick={() => props.onFormSwitch('login')}>Already have an account? Login here.</button>
-    </div>
+        <div>
+            <h1>NEON NIGHTBEAT</h1>
+            <div className="auth-form-container">
+                <h2>REGISTER</h2>
+                <form className="form" onSubmit={handleSubmit}>
+                    <input value={username} onChange={(e) => setUsername(e.target.value)}type="text" placeholder="Username" id="username" name="username" />
+                    <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="Password" id="password" name="password" />
+                    <button className="register-btn" type="submit">REGISTER</button>
+                </form>
+                <button className="link-btn" onClick={() => props.onFormSwitch('login')}>Already have an account? Login here.</button>
+            </div>
+        </div>
+        
     )
 }
